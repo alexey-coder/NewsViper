29b7b0d908056f15016b2405f335745217eb516e
Alexey <alex_coder@icloud.com>
2020-03-24 22:45:24 +0300

    Initial Commit

---
+++ viper-rss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viper-rss
+//
+//  Created by user on 24.03.2020.
+//  Copyright © 2020 smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
